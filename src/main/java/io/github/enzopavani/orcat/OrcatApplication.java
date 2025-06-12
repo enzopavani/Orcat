033afc5aed91085ec5fa7322e74350abeb27aafb
Enzo <tijoloenso@gmail.com>
2025-06-11 21:56:29 -0300

    Projeto criado

---
+++ src/main/java/io/github/enzopavani/orcat/OrcatApplication.java
@@ -0,0 +1,13 @@
+package io.github.enzopavani.orcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/enzopavani/orcat/OrcatApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.enzopavani.orcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
